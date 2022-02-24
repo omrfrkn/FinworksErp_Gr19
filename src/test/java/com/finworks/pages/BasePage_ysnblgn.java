090724df9a9dee9d3b107022c82025ba6e528d25
ysnblgn <yasinbilgin14@gmail.com>
2022-02-24 20:40:10 +0300

    BasePage_ysnblgn is done

---
+++ src/test/java/com/finworks/pages/BasePage_ysnblgn.java
@@ -0,0 +1,40 @@
+package com.finworks.pages;
+
+import com.finworks.utilities.BrowserUtils;
+import com.finworks.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * @author ybilgin
+ * @project FinworksErp
+ */
+
+
+public abstract class BasePage_ysnblgn {
+
+    public BasePage_ysnblgn() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(className = "oe_topbar_name")
+    public WebElement userName;
+
+    @FindBy(linkText = "Log out")
+    public WebElement logout;
+
+    public String getUserName(){
+        return userName.getText();
+    }
+
+    public void navigateTo(String tab){
+        String locator = "//div[@class='navbar-collapse collapse']//a[contains(.,'"+ tab +"')]";
+        BrowserUtils.waitForClickablility(By.xpath(locator),5);
+        Driver.get().findElement(By.xpath(locator)).click();
+    }
+
+
+
+}
